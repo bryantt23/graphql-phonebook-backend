3f9eb3bd7b69838f6ea576089a7a98af1919befb
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-12 18:52:21 -0800

    https://fullstackopen.com/en/part8/react_and_graph_ql

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server');
 
 let persons = [
   {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    street: 'Tapiolankatu 5 A',
+    city: 'Espoo',
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431'
   },
   {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    street: 'Malminkaari 10 A',
+    city: 'Helsinki',
     id: '3d599470-3436-11e9-bc57-8b80ba54c431'
   },
   {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
+    name: 'Venla Ruuska',
+    street: 'Nallemäentie 22 C',
+    city: 'Helsinki',
     id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-  },
-]
+  }
+];
 
 const typeDefs = gql`
   type Person {
@@ -33,7 +33,7 @@ const typeDefs = gql`
 
   type Address {
     street: String!
-    city: String! 
+    city: String!
   }
 
   type Query {
@@ -41,30 +41,29 @@ const typeDefs = gql`
     allPersons: [Person!]!
     findPerson(name: String!): Person
   }
-`
+`;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+    findPerson: (root, args) => persons.find(p => p.name === args.name)
   },
   Person: {
-    address: (root) => {
-      return { 
+    address: root => {
+      return {
         street: root.street,
         city: root.city
-      }
+      };
     }
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-})
+  resolvers
+});
 
 server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  console.log(`Server ready at ${url}`);
+});
